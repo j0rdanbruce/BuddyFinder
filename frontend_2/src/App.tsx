39a7af2e571ded2efa22bcf70981cb707cab9844
Jordan Bruce <jeb79@njit.edu>
2024-08-22 14:12:36 -0400

    Finished the api for finding all available pets for adoption.

---
+++ frontend_2/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+//component imports go here
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
